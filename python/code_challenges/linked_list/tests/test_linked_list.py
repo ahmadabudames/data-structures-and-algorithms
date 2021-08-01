b56428fdfb44113a1dbea4ca093965ec76dd5fa2
ahmadabudames <ahmadabudamesmais1999@gmail.com>
2021-08-01 18:44:24 +0300

    finshed

--- python/code_challenges/linked_list/tests/test_linked_list.py
+++ python/code_challenges/linked_list/tests/test_linked_list.py
@@ -1,3 +1,4 @@
+from linked_list import __version__
 from linked_list.linked_list import (LinkedList,Node)
 
 
@@ -71,6 +72,74 @@ def test_all_collection_values_in_in_the_linedlist():
     actual=test_6.__str__()
     expected='(ahmad) -> (shahar) -> (ameen) -> (abudames) -> none'
     assert actual==expected
+
+def test_end_of_the_linked_list():
+    test_append=LinkedList()
+    test_append.insert(22)
+    test_append.insert(4)
+    test_append.append(2)
+    actual=test_append.__str__()
+    expected='(4) -> (22) -> (2) -> none'
+    assert actual==expected
+
+
+def test_multiple_nodes_to_the_end_of_a_linked_list():
+    test_append=LinkedList()
+    test_append.append(2)
+    test_append.append(1)
+    actual=test_append.__str__()
+    expected='(2) -> (1) -> none'
+    assert actual==expected
+
+def test_before_middle_of_the_linked_list():
+    test_before=LinkedList()
+    test_before.append("ahmad")
+    test_before.append("shaher")
+    test_before.append("abudames")
+    test_before.insertBefore("abudames","ameen")
+    actual=test_before.__str__()
+    expected='(ahmad) -> (shaher) -> (ameen) -> (abudames) -> none'
+    assert actual==expected
+
+def test_before_the_first_node_of_a_linked_list():
+     test_before_first=LinkedList()
+     test_before_first.append(2)
+     test_before_first.append(3)
+     test_before_first.insertBefore(2,1)
+     actual=test_before_first.__str__()
+     expected='(1) -> (2) -> (3) -> none'
+     assert actual==expected
+
+
+def test_insert_after():
+     test_after=LinkedList()
+     test_after.append(1)
+     test_after.append(3)
+     test_after.insertAfter(1,2)
+     actual=test_after.__str__()
+     expected='(1) -> (2) -> (3) -> none'
+     assert actual==expected
+
+def test_insert_after_the_last_node():
+    test_after_last_node=LinkedList()
+    test_after_last_node.append(1)
+    test_after_last_node.append(2)
+    test_after_last_node.insertAfter(2,3)
+    actual=test_after_last_node.__str__()
+    expected='(1) -> (2) -> (3) -> none'
+    assert actual==expected
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
