ac296c65d4ea1e4ce2f4c305e4b950124b1d7e12
ahmadabudames <ahmadabudamesmais1999@gmail.com>
2021-09-11 15:53:48 +0300

    finished

---
+++ python/code_challenges/tree_intersection/tree_intersection/hashMap.py
@@ -0,0 +1,57 @@
+from tree_intersection.linkedList import *
+
+
+class HashTable:
+
+  def __init__(self,size=720) -> None:
+      self.bucket=[None]* size
+      self.size= size
+
+
+  def add(self,key,value):
+    idx = self.hash(key)
+
+    if self.bucket[idx]== None:
+      self.bucket[idx]=LinkedList()
+    self.bucket[idx].add([key,value])
+
+
+
+
+  def get(self,key):
+
+
+    idx = self.hash(key)
+    if self.bucket[idx]== None:
+      return None
+    else:
+      current = self.bucket[idx].head
+      while current:
+        if current.value[0] == key:
+            return current.value[1]
+        current = current.next
+
+
+
+  def contains(self,key):
+
+    idx = self.hash(key)
+    if self.bucket[idx] == None:
+      return False
+    else:
+      current = self.bucket[idx].head
+      while current:
+        if current.value[0] == key:
+            return True
+        else:
+          return False
+
+
+
+  def hash(self,key):
+
+    value= 0
+    for letter in key:
+      value+= ord(letter)
+    index = value * 10 % self.size
+    return index
